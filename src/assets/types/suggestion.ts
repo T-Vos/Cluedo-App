6817b51886f368732da6f8b4956a641ac8d2ffb8
Thomas Vos <31988100+T-Vos@users.noreply.github.com>
2025-04-14 13:31:47 +0200

    rewrite to next and rust

--- src/assets/types/suggestion.ts
+++
@@ -1,8 +0,0 @@
-export interface Suggestion {
-  userName: string;
-  supspect: string;
-  weapon: string;
-  room: string;
-  refuted_By_User: string;
-  refuted_Card: string;
-}
--- src/assets/types/user.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-  userId: string;
-  userName: string;
-  cards: number;
-  postion: number;
-}
